98b3083bf37454945d7730782e8b90ce0e777336
bruno <brunokitice@gmail.com>
2025-03-26 12:44:02 -0300

    adicionando novos casos de testes -> formatacao de campos | validacoes de dados aceitos nos campos | mensagem de erro quando algo se torna obrigatorio e nao e preenchido | utilizando clear apos o preenchimento

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,131 +1,106 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
   beforeEach(() => {
-    cy.visit('src/index.html')
-  })
+    cy.visit('src/index.html');
+  });
 
   it('verifica o título da aplicação', () => {
-    cy.title()
-      .should('eq', 'Central de Atendimento ao Cliente TAT')
-  })
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
+  });
 
-  it('preenche os campos, envia formulário e valida mensagem de sucesso',
-  () => {
-      /// preenche nome e compara se é o valor esperado
-      cy.get('#firstName')
-        .as('nome')
-        .type('Bruno')
-      cy.get('@nome')
-        .should('have.value', 'Bruno')
+  it('preenche os campos, envia formulário e valida mensagem de sucesso', () => {
+    /// funcionalidades cypress
+    const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz0123456789', 10);
+    /// preenche nome e compara se é o valor esperado
+    cy.get('#firstName').as('nome').type('Bruno');
+    cy.get('@nome').should('have.value', 'Bruno');
+    /// preenche sobrenome e compara se é o valor esperado
+    cy.get('#lastName').as('sobrenome').type('Kitice');
+    cy.get('@sobrenome').should('have.value', 'Kitice');
+    /// preenche email e compara se é o valor esperado
+    cy.get('#email').as('email').type('brunokitice@gmail.com');
+    cy.get('@email').should('have.value', 'brunokitice@gmail.com');
+    /// preenche telefone e compara se é o valor esperado
+    cy.get('#phone').as('phone').type('34996344406');
+    cy.get('@phone').should('have.value', '34996344406');
+    /// Select an <option> within a <select>
+    cy.get('#product').as('blog').select('blog');
+    cy.get('@blog').should('have.value', 'blog');
+    cy.get('#product').as('cursos').select('cursos');
+    cy.get('@cursos').should('have.value', 'cursos');
+    cy.get('#product').as('mentoria').select('mentoria');
+    cy.get('@mentoria').should('have.value', 'mentoria');
+    cy.get('#product').as('youtube').select('youtube');
+    cy.get('@youtube').should('have.value', 'youtube');
+    /// click checkbox or radio elements
+    /// Click a DOM element
+    /// By default, Cypress will error if you're trying to click multiple elements. By passing { multiple: true } Cypress will iteratively apply the click to each element and will also log to the Command Log multiple times
+    cy.get('#support-type > :nth-child(2) > input').as('ajuda');
+    cy.get('@ajuda')
+      .click({
+        multiple: true,
+      })
+      .should('have.value', 'ajuda');
+    cy.get(':nth-child(3) > input').as('elogio');
+    cy.get('@elogio')
+      .click({
+        multiple: true,
+      })
+      .should('have.value', 'elogio');
+    cy.get(':nth-child(4) > input').as('feedback');
+    cy.get('@feedback')
+      .click({
+        multiple: true,
+      })
+      .should('have.value', 'feedback');
+    /// click checkbox or radio elements
+    cy.get('#email-checkbox').as('email');
+    cy.get('@email').click().should('have.value', 'email');
+    cy.get('#phone-checkbox').as('phone');
+    cy.get('@phone').click().should('have.value', 'phone');
+    /// Type into a DOM element
+    cy.get('#open-text-area').as('textArea');
+    cy.get('textArea').type(longText, { delay: 0 }).should('have.value', longText);
+    /// Click no botão de envio
+    cy.get('.button[type="submit"]').as('enviar').click();
+    /// verifica se a mensagem de sucesso está visível
+    cy.get('.success').as('msgsucesso').should('be.visible');
+    /// valida a mensagem de sucesso
+  });
 
-      /// preenche sobrenome e compara se é o valor esperado
-      cy.get('#lastName')
-        .as('sobrenome')
-        .type('Kitice')
-      cy.get('@sobrenome')
-        .should('have.value', 'Kitice')
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').as('nome').type('Bruno');
+    cy.get('#lastName').as('sobrenome').type('Kitice');
+    cy.get('#email').as('email').type('brunokitice');
+    cy.get('#phone').as('phone').type('34996344406');
+    cy.get('#open-text-area').as('textArea');
+    cy.get('textArea').type('teste');
+    cy.get('.button[type="submit"]').as('enviar').click();
+    cy.get('.error').as('msgerror').should('be.visible');
+  });
 
-      /// preenche email e compara se é o valor esperado
-      cy.get('#email')
-        .as('email')
-        .type('brunokitice@gmail.com')
-      cy.get('@email')
-        .should('have.value', 'brunokitice@gmail.com')
+  it('verifica se o telefone só aceita números', () => {
+    cy.get('#phone').as('phone').type('qwrqwerzfasdfa.,-+').should('have.value', '');
+  });
 
-      /// preenche telefone e compara se é o valor esperado
-      cy.get('#phone')
-        .as('phone')
-        .type('34996344406')
-      cy.get('@phone')
-        .should('have.value', '34996344406')
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').as('nome').type('Bruno');
+    cy.get('#lastName').as('sobrenome').type('Kitice');
+    cy.get('#email').as('email').type('brunokitice@gmail.com');
+    cy.get('#phone-checkbox').as('phone').click();
+    cy.get('#open-text-area').as('textArea');
+    cy.get('textArea').type('teste');
+    cy.get('.button[type="submit"]').as('enviar').click();
+    cy.get('.error').as('msgerror').should('be.visible');
+  });
 
-      /// Select an <option> within a <select>
-      cy.get('#product')
-        .as('blog')
-        .select('blog')
-      cy.get('@blog')
-        .should('have.value', 'blog')
-
-      cy.get('#product')
-        .as('cursos')
-        .select('cursos')
-      cy.get('@cursos')
-        .should('have.value', 'cursos')
-
-      cy.get('#product')
-        .as('mentoria')
-        .select('mentoria')
-      cy.get('@mentoria')
-        .should('have.value', 'mentoria')
-
-      cy.get('#product')
-        .as('youtube')
-        .select('youtube')
-      cy.get('@youtube')
-        .should('have.value', 'youtube')
-
-      /// Check checkbox or radio elements
-      /// Click a DOM element
-      /// By default, Cypress will error if you're trying to click multiple elements. By passing { multiple: true } Cypress will iteratively apply the click to each element and will also log to the Command Log multiple times
-      cy.get('#support-type > :nth-child(2) > input')
-        .as('ajuda')
-      cy.get('@ajuda')
-        .check({
-          multiple: true,
-        })
-        .should('have.value', 'ajuda')
-
-      cy.get(':nth-child(3) > input')
-        .as('elogio')
-      cy.get('@elogio')
-        .check({
-          multiple: true,
-        })
-        .should('have.value', 'elogio')
-
-      cy.get(':nth-child(4) > input')
-        .as('feedback')
-      cy.get('@feedback')
-        .check({
-          multiple: true,
-        })
-        .should('have.value', 'feedback')
-
-      /// Check checkbox or radio elements
-      cy.get('#email-checkbox')
-        .as('email')
-      cy.get('@email')
-        .check()
-        .should('have.value', 'email')
-
-      cy.get('#phone-checkbox')
-        .as('phone')
-      cy.get('@phone')
-        .check()
-        .should('have.value', 'phone')
-
-      /// Type into a DOM element
-      cy.get('#open-text-area')
-        .as('textArea')
-      cy.get('textArea')
-        .type(
-          'Escrevendo em uma text-area com cypress.\nAAAAAAAAAAAAAAAAAAAAAA'
-        )
-        .should('have.value',
-          'Escrevendo em uma text-area com cypress.\nAAAAAAAAAAAAAAAAAAAAAA'
-          )
-
-      /// Click no botão de envio
-      cy.get('.button')
-        .as('enviar')
-        .click()
-
-      /// verifica se a mensagem de sucesso está visível
-      cy.get('.success > strong')
-        .as('msgsucesso')
-        .should('be.visible')
-
-      /// valida a mensagem de sucesso
-      cy.get('@msgsucesso')
-        .should('have.text', 'Mensagem enviada com sucesso.')
-    })
-})
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName').as('nome').type('Bruno').should('have.value', 'Bruno').clear();
+    cy.get('#lastName').as('sobrenome').type('Kitice').should('have.value', 'Kitice').clear();
+    cy.get('#email')
+      .as('email')
+      .type('brunokitice@gmail.com')
+      .should('have.value', 'brunokitice@gmail.com')
+      .clear();
+    cy.get('#phone').as('phone').type('34996344406').should('have.value', '34996344406').clear();
+  });
+});
